100cf7556c9d5341f3f5fbfde934dec0deb1ce45
riz8 <rizach8@gmail.com>
2020-11-15 16:03:54 +0100

    Example, drawing a line

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
-﻿int main()
+﻿#include <SFML/Graphics.hpp>
+
+int main()
 {
+	sf::RenderWindow window(sf::VideoMode(500, 500), "Test Window");
+
+	sf::Vertex line1[] =
+	{
+		sf::Vertex(sf::Vector2f(50.f, 50.f)),
+		sf::Vertex(sf::Vector2f(100.f, 100.f))
+	};
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(line1, 2, sf::Lines);
+		window.display();
+	}
+
 	return 0;
 }
