8a02bfe7a4556aab42a34ffbe5f18f2d4d0185a4
riz8 <rizach8@gmail.com>
2020-11-15 16:33:44 +0100

    Background indication when points touch each other

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 ﻿#include <SFML/Graphics.hpp>
 
+bool PointCollidePoint(const sf::Vector2f& a, const sf::Vector2f& b)
+{
+	if (a.x == b.x && a.y == b.y)
+		return true;
+
+	return false;
+}
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Test Window");
 
-	sf::Vertex line1[] =
-	{
-		sf::Vertex(sf::Vector2f(50.f, 50.f)),
-		sf::Vertex(sf::Vector2f(100.f, 100.f))
-	};
+	sf::Color background_color = sf::Color::Black;
+
+	sf::Vertex mousePoint(sf::Vector2f(10.f, 10.f));
+	sf::Vertex targetPoint(sf::Vector2f(250.f, 250.f));
 
 	while (window.isOpen())
 	{
@@ -17,10 +24,26 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			else if (event.type == sf::Event::MouseMoved)
+			{
+				auto new_position = sf::Vector2f{ static_cast<float>(event.mouseMove.x - 20), static_cast<float>(event.mouseMove.y - 20) };
+				mousePoint.position = new_position;
+			}
 		}
 
-		window.clear();
-		window.draw(line1, 2, sf::Lines);
+		if (PointCollidePoint(mousePoint.position, targetPoint.position))
+		{
+			background_color = sf::Color::Red;
+		}
+		else
+		{
+			background_color = sf::Color::Black;
+		}
+
+		window.clear(background_color);
+		window.draw(&targetPoint, 1, sf::Points);
+		window.draw(&mousePoint, 1, sf::Points);
 		window.display();
 	}
 
