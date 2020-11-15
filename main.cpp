243aa2c4ace2afdb6427ea68b897f89abb286328
riz8 <rizach8@gmail.com>
2020-11-15 16:48:12 +0100

    Highlight background when cursor point hits the circle

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <cmath>
 
 bool PointCollidePoint(const sf::Vector2f& a, const sf::Vector2f& b)
 {
 	if (a.x == b.x && a.y == b.y)
+		return true;
+
+	return false;
+}
+
+bool PointCollideCircle(const sf::Vector2f& p, const sf::CircleShape& c)
+{
+	// SFML objects origo is top left
+	float delta_x = p.x - c.getPosition().x - c.getRadius();
+	float delta_y = p.y - c.getPosition().y - c.getRadius();
+
+	// Pythagoras to find distance between point and circle center
+	float distance = std::sqrt((delta_x*delta_x) + (delta_y*delta_y));
+
+	if (distance <= c.getRadius())
 		return true;
 
 	return false;
@@ -15,7 +31,11 @@ int main()
 	sf::Color background_color = sf::Color::Black;
 
 	sf::Vertex mousePoint(sf::Vector2f(10.f, 10.f));
-	sf::Vertex targetPoint(sf::Vector2f(250.f, 250.f));
+	
+	const float radius = 15.f;
+	sf::CircleShape circle(radius*2);
+	circle.setFillColor(sf::Color::Green);
+	circle.setPosition(250.f - radius, 250.f - radius);
 
 	while (window.isOpen())
 	{
@@ -27,12 +47,12 @@ int main()
 
 			else if (event.type == sf::Event::MouseMoved)
 			{
-				auto new_position = sf::Vector2f{ static_cast<float>(event.mouseMove.x - 20), static_cast<float>(event.mouseMove.y - 20) };
+				auto new_position = sf::Vector2f{ static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y) };
 				mousePoint.position = new_position;
 			}
 		}
 
-		if (PointCollidePoint(mousePoint.position, targetPoint.position))
+		if (PointCollideCircle(mousePoint.position, circle))
 		{
 			background_color = sf::Color::Red;
 		}
@@ -42,8 +62,8 @@ int main()
 		}
 
 		window.clear(background_color);
-		window.draw(&targetPoint, 1, sf::Points);
 		window.draw(&mousePoint, 1, sf::Points);
+		window.draw(circle);
 		window.display();
 	}
 
